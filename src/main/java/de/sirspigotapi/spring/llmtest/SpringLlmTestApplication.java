682c4834f8b750414aaf28c529832f2c90aeddfc
SirSpigotAPI <tonidenk1@gmail.com>
2024-03-23 14:13:12 +0100

    Initial commit

---
+++ src/main/java/de/sirspigotapi/spring/llmtest/SpringLlmTestApplication.java
@@ -0,0 +1,13 @@
+package de.sirspigotapi.spring.llmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLlmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLlmTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/sirspigotapi/spring/llmtest/SpringLlmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sirspigotapi.spring.llmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLlmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
